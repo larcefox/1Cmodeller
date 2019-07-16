27f2204527981479d5357c3afb1484d1d8cdc0fb
larcefox <larcethefox@gmail.com>
2019-07-16 08:37:08 +0300

    using clases with same functionality 190711 Создан списокс объектов класса MainObject1C 190712 Без изменений 190713 Отображаются модели по количеству объектов 190716 Отображение основных объектов в 3d
    //Необходимо создать до цикла первый элемент дерева, чтобы последовательно собрать в него все свойства\
    //и в сотальные объекты следом.
    //Разобрать

--- pandaViewer.py
+++ pandaViewer.py
@@ -17,21 +17,42 @@ class MainObjects(ShowBase):
         self.acceptOnce('q',exit)
 
         self.mainVisualObj = dict()
+        self.mainRegistryText = dict()
+        self.text = dict()
+
         self.x = -60
         self.y = 150
         self.z = 0
 
-        font = self.loader.loadFont('arial.ttf')
-
+        self.font = self.loader.loadFont('17430.otf')
+        # self.text.setText('This is the text')
+        # self.textNodePath = self.aspect2d.attachNewNode(self.text)
+        # self.textNodePath.setScale(1)
+        # self.textNodePath.setPos(-62 , 149, 0)
+        # self.textNodePath.reparentTo(self.mainObjects)
 
         with codecs.open("data.txt", 'rU', 'utf-16') as f_obj:
             mainObjs1C, objs1C = dataPars.csv_dict_reader(f_obj)
 
         for key in list(mainObjs1C.keys()):
+            randx, randy, randz = random.randint(-5, 5), random.randint(85, 95), random.randint(-5, 5)
             self.mainVisualObj[key] = self.loader.loadModel("1Cregistr.egg")
-            self.mainVisualObj[key].setHpr(random.randint(-5, 5), random.randint(85, 95), random.randint(-5, 5))
+            self.mainVisualObj[key].setHpr(randx, randy, randz)
             self.mainVisualObj[key].setPos(self.x, self.y, self.z)
             self.mainVisualObj[key].reparentTo(self.mainObjects)
+            self.mainVisualObj[key].setTwoSided(True)
+
+            self.text[key] = TextNode('registry text node')
+            self.text[key].setTextColor(1, 0, 0, 1)
+            self.text[key].setFont(self.font)
+
+            self.text[key].setText(key)
+            self.mainRegistryText[key] = self.aspect2d.attachNewNode(self.text[key])
+            self.mainRegistryText[key].setScale(1)
+            self.mainRegistryText[key].setPos(-4, 0, 1)
+            self.mainRegistryText[key].setHpr(0, randy+180, 0)
+            self.mainRegistryText[key].reparentTo(self.mainVisualObj[key])
+            print(key)
             self.x += 15
 
             if self.x > 60:
